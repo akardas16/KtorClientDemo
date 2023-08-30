3bb3a641e101045ca796155b362b95ea19127952
akardas16 <krdsabdllh53@gmail.com>
2023-08-30 18:27:52 +0200

    updated

--- app/src/main/java/com/hiteshchopra/ktorclientdemo/data/ktornetworking/Endpoints.kt
+++ app/src/main/java/com/hiteshchopra/ktorclientdemo/data/ktornetworking/Endpoints.kt
@@ -1,8 +1,9 @@
 package com.hiteshchopra.ktorclientdemo.data.ktornetworking
 
+import com.hiteshchopra.ktorclientdemo.BuildConfig
+
 object Endpoints {
-  private const val BASE_URL = "https://reqres.in"
-  const val FETCH_IMAGES_URL = "$BASE_URL/photos?page=1"
-   fun allUsers() = "$BASE_URL/api/users?page=2"
-    fun createUserUrl() = "$BASE_URL/api/users"
+  const val FETCH_IMAGES_URL = "${BuildConfig.BASE_URL}/photos?page=1"
+   fun allUsers() = "${BuildConfig.BASE_URL}/api/users?page=2"
+   fun createUserUrl() = "${BuildConfig.BASE_URL}/api/users"
 }
